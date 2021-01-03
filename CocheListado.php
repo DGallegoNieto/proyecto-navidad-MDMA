0373c8bb62beb22301ad9287b14dbe7bad477595
David Gallego <dgallegonieto@educa.madrid.org>
2021-01-03 12:05:34 +0100

    Listado de coche y diseño

--- CocheListado.php
+++ CocheListado.php
@@ -1,1 +1,49 @@
 <?php
+require_once "_varios.php";
+
+$pdo = obtenerPdoConexionBD();
+
+$sql = "SELECT * FROM coche ORDER BY idCoche";
+$select = $pdo->prepare($sql);
+$select->execute([]);
+$rs = $select->fetchAll();
+
+
+?>
+<html>
+<head>
+    <meta charset="UTF-8">
+</head>
+<body>
+<h1>Listado de coches</h1>
+
+<table border="1">
+
+    <tr>
+        <th>Id</th> <!--Numero de serie?-->
+        <th>Marca</th>
+        <th>Modelo</th>
+        <th>Tipo</th>
+        <th>Precio</th>
+    </tr>
+    <?php
+    foreach ($rs as $fila) { ?>
+        <tr>
+            <td> <p><?=$fila["idCoche"] ?></p></td>
+            <td> <p><?=$fila["marca"] ?></p></td>
+            <td> <p><?=$fila["modelo"] ?></p></td>
+            <td> <p><?=$fila["tipo"] ?></p></td>
+            <td> <p><?=$fila["precio"] ?>€</p></td>
+            <td><input type="radio" name="coche" </td>
+        </tr>
+    <?php } ?>
+
+</table>
+<br/>
+<a href="">Volver al inicio</a>
+<br/>
+<a href="">Siguiente</a>
+
+</body>
+
+</html>
--- DisenioListado.php
+++ DisenioListado.php
@@ -1,1 +1,51 @@
 <?php
+require_once "_varios.php";
+
+$pdo = obtenerPdoConexionBD();
+
+$sql = "SELECT * FROM disenio ORDER BY idDisenio";
+$select = $pdo->prepare($sql);
+$select->execute([]);
+$rs = $select->fetchAll();
+
+
+?>
+<html>
+<head>
+    <meta charset="UTF-8">
+</head>
+<body>
+<h1>Listado de diseños</h1>
+
+<table border="1">
+
+    <tr>
+        <th>Id</th>
+        <th>Acabado</th>
+        <th>Llantas</th>
+        <th>Asientos</th>
+        <th>Parrilla</th>
+        <th>Precio</th>
+    </tr>
+    <?php
+    foreach ($rs as $fila) { ?>
+        <tr>
+            <td> <p><?=$fila["idDisenio"] ?></p></td>
+            <td> <p><?=$fila["acabado"] ?></p></td>
+            <td> <p><?=$fila["llantas"] ?></p></td>
+            <td> <p><?=$fila["asientos"] ?></p></td>
+            <td> <p><?=$fila["parrilla"] ?></p></td>
+            <td> <p><?=$fila["precio"] ?>€</p></td>
+            <td><input type="radio" name="disenio" </td>
+        </tr>
+    <?php } ?>
+
+</table>
+<br/>
+<a href="">Atrás</a>
+<br/>
+<a href="">Siguiente</a>
+
+</body>
+
+</html>
