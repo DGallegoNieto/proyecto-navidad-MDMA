5217efcd13e1221328ed8df0c402a3586295a562
mariogarcia5 <72387652+mariogarcia5@users.noreply.github.com>
2021-01-07 13:57:41 +0100

    Add files via upload
    Inicio e imágenes

--- Inicio.php
+++ Inicio.php
@@ -11,6 +11,51 @@
 //editar coche, motor, garantia, disenio...
 ?>
 <html>
+<style type="text/css">
+    div1 { position: absolute; top: 30%; left: 3%; border: 5px solid black; text-align: center; background-color: darkgrey}
+    div2 { position: absolute; top: 30%; left: 32%; border: 5px solid black; text-align: center; background-color: darkgrey}
+    div3 { position: absolute; top: 30%; left: 63%; border: 5px solid black; text-align: center; background-color: darkgrey}
 
-<a href="SesionInicioMostrarFormulario.php">Iniciar sesión</a>
+</style>
+
+<h3 align="right"><a href="SesionInicioMostrarFormulario.php">Iniciar sesión</a></h3>
+<h1><b><u>CONCESIONARIO DE COCHES</u></b></h1>
+<H3>CREA TU PROPIO COCHE PERSONALIZADO <a href="CocheListado.php" >AQUÍ</a></H3>
+<h3>COCHES MÁS VENDIDOS:</h3>
+<div1>
+<b><u>VOLKSWAGEN TIGUAN</u></b>
+</br>
+    <p><b>COLOR:</b> AZÚL</p>
+    <p><b>DISEÑO:</b> DEPORTIVO (R-LINE)</p>
+    <p><b>GARANTÍA:</b> 3 AÑOS</p>
+    <p><b>MOTOR:</b> GASOLINA 150CV MANUAL</p>
+    <p><b>PRECIO: 32.500€</b> </p>
+    <img src="volkswagen.jfif" width="248px">
+    </br></br>
+</div1>
+
+<div2>
+    <b><u>AUDI A4</u></b>
+    </br>
+    <p><b>COLOR:</b> ROJO</p>
+    <p><b>DISEÑO:</b> INTERMEDIO (AMBITION)</p>
+    <p><b>GARANTÍA:</b> 4 AÑOS</p>
+    <p><b>MOTOR:</b> DIESEL 190CV AUTOMÁTICO</p>
+    <p><b>PRECIO: 28.900€</b> </p>
+    <img src="audiA4.jfif" width="301px">
+    </br></br>
+</div2>
+
+<div3>
+    <b><u>SEAT CORDOBA</u></b>
+    </br>
+    <p><b>COLOR:</b> METÁTICO</p>
+    <p><b>DISEÑO:</b> BASE (ACTIVE)</p>
+    <p><b>GARANTÍA:</b> 3 AÑOS</p>
+    <p><b>MOTOR:</b> DIESEL 150CV MANUAL</p>
+    <p><b>PRECIO: 19.500€</b> </p>
+    <img src="seatCordoba.jfif" width="228px">
+    </br></br>
+</div3>
+
 </html>
